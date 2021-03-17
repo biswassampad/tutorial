5961476776fb6194cc072a51dd7fb036bdadee0d
Biswas Sampad <biswa.satpathy@iserveu.in>
2021-03-17 16:52:18 +0530

    promise async

--- controller/index.js
+++ controller/index.js
@@ -1,6 +1,34 @@
-exports.makename = (req, res) => {
+const { validationResult } = require('express-validator');
+const _this = this;
+exports.makename = async(req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.json({
+            status: 1,
+            msg: "Incomplete request payload",
+            errors: errors.array()
+        })
+    }
     const name = req.body.name;
     const surname = req.body.surname;
-    let fullname = name + ' ' + surname;
-    res.json({ name: fullname });
+    const fullname = name + ' ' + surname;
+    const age = req.body.age;
+
+    await _this.validation(age)
+        .then((eligibility) => {
+            res.json({ name: fullname, status: eligibility })
+        })
+        .catch((error) => {
+            res.json({ name: fullname, error: error })
+        })
+}
+
+exports.validation = async(age) => {
+    return new Promise((resolve, reject) => {
+        if (age > 18) {
+            resolve('Eligible')
+        } else {
+            reject('Not Eligible')
+        }
+    })
 }
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
+const { body } = require('express-validator');
 const controller = require('../controller/index');
-router.post('/getname', controller.makename);
+router.post('/getname', [
+    body('name').notEmpty().withMessage("name should not be empty"),
+    body('surname').notEmpty().withMessage("surname should not be empty"),
+    body('age').notEmpty().withMessage("age should not be empty")
+], controller.makename);
 
 module.exports = router;
